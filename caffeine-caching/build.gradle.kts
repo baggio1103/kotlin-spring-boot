c13afc2a23cf67a95d24dbf91d6d02fc8db4dcc2
baggio1103 <valijanov1103@mail.ru>
2024-01-29 17:39:37 +0500

    Add actuator and micrometer for observing

--- caffeine-caching/build.gradle.kts
+++ caffeine-caching/build.gradle.kts
@@ -21,8 +21,9 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
-    implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
